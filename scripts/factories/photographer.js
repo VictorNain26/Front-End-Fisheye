a069c1d565009e5860d112706200073c1f115408
Victor <victor.lenain26@gmail.com>
2022-03-24 14:57:15 +0100

    add fetch data from json

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,30 @@
-function photographerFactory(data) {
-    const { name, portrait } = data;
+export default function photographerFactory(data) {
+  const { name, portrait, id, country, city, price, tagline } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+  const picture = `assets/photographers/${portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+  function getUserCardDOM() {
+      const a = document.createElement( 'a' );
+      const article = document.createElement( 'article' );
+      const img = document.createElement( 'img' );
+      img.setAttribute("src", picture)
+      const h2 = document.createElement( 'h2' );
+      const pCityCountry = document.createElement( 'p' )
+      const pTagline = document.createElement( 'p' )
+      const pPrice = document.createElement( 'p' )
+      pCityCountry.textContent = `${city}, ${country}`
+      pTagline.textContent = tagline
+      pPrice.textContent = `${price}Є/jour`
+      h2.textContent = name;
+      article.appendChild(img);
+      article.appendChild(h2);
+      article.appendChild(pCityCountry);
+      article.appendChild(pTagline);
+      article.appendChild(pPrice);
+      a.appendChild(article)
+      a.href = `${window.location}photographer.html?id=${id}`
+      return (a);
+  }
+
+  return { getUserCardDOM }
 }
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,32 @@
-//Mettre le code JavaScript lié à la page photographer.html
+const getPhotographers = async () => {
+    const photographerId = (new URL(document.location)).searchParams.get("id");
+
+    const  medias = await fetch('../../data/photographers.json')
+            .then(res => res.json())
+            .then(data => data.media.filter(ele => ele.photographerId === parseInt(photographerId)));
+
+    const photographer = await fetch('../../data/photographers.json')
+            .then(res => res.json())
+            .then(data => data.photographers.filter(ele => ele.id === parseInt(photographerId)).shift());
+
+    return { photographer, medias }
+}
+
+function displayData(photographers) {
+    const photographersSection = document.querySelector(".photographer_section");
+
+
+    photographers.forEach((photographer) => {
+        const photographerModel = photographerFactory(photographer);
+        const userCardDOM = photographerModel.getUserCardDOM();
+        photographersSection.appendChild(userCardDOM);
+    });
+};
+
+async function init() {
+    const {photographer, medias} = await getPhotographers();
+    console.log(photographer, medias);
+    // displayData(photographers);
+};
+
+init();
